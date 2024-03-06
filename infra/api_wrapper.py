267189cdaa5c26d75bf45f217468a035fad29e16
MohammadKhayyo <mohammadkhayyo@gmail.com>
2024-03-06 22:48:17 +0200

    first commit

---
+++ infra/api_wrapper.py
@@ -0,0 +1,20 @@
+import requests
+
+
+class APIWrapper:
+
+    def __init__(self):
+        self.response = None
+        url = None
+        self.my_request = requests
+
+    def api_get_request(self, url):
+        self.response = self.my_request.get(url)
+        return self.response
+
+    def api_post_request(self, url):
+        self.response = self.my_request.get(url)
+        if self.response.ok:
+            return self.response
+        else:
+            return self.response.status_code
